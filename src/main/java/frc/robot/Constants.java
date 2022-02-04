b5ecfb71c6dfbc943e10ba059ac21bbaf390ab64
lnstemprogramming <lnstempunks@gmail.com>
2022-02-04 18:47:21 -0500

    added SparkMax vendor library, created Sparkmaxes, differential drive in Drivetrain

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,16 +3,16 @@ package frc.robot;
 // Define Enumerated Class
 // This class is going to store all of the constant variables
 public class Constants {
+    // PWM ports
     public static final int shooterMotor1 = 0;
     public static final int shooterMotor2 = 1;
-    public static final int driveMotor6 = 2;
-    public static final int driveMotor1 = 3;
-    public static final int driveMotor2 = 4;
-    public static final int driveMotor3 = 5;
     public static final int indexMotor = 6;
     public static final int limitSwitch = 7;
-
-
+    // CAN bus ports
+    public static final int leftFront = 0;
+    public static final int rightFront = 1;
+    public static final int leftBack = 2;
+    public static final int rightBack = 3;
 
 
 
--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -1,9 +1,18 @@
 package frc.robot;
 import frc.robot.Constants;
-
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 public class Drivetrain {
-    
+    CANSparkMax leftFront = new CANSparkMax(Constants.leftFront, CANSparkMaxLowLevel.MotorType.kBrushless);
+    CANSparkMax rightFront = new CANSparkMax(Constants.rightFront, CANSparkMaxLowLevel.MotorType.kBrushless);    
+    CANSparkMax leftBack = new CANSparkMax(Constants.leftBack, CANSparkMaxLowLevel.MotorType.kBrushless);
+    CANSparkMax rightBack = new CANSparkMax(Constants.rightBack, CANSparkMaxLowLevel.MotorType.kBrushless);
+    MotorControllerGroup leftController = new MotorControllerGroup(leftFront, leftBack);
+    MotorControllerGroup rightController = new MotorControllerGroup(rightFront, rightBack);
+    DifferentialDrive m_drive = new DifferentialDrive(leftController, rightController);
 }
 // differential drive
 // arcade drive
